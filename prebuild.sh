0a818a46e8173ddea9016482205666c5fcae4093
Jannis R <mail@jannisr.de>
2022-05-10 23:49:45 +0200

    prebuild.sh: use run prebuildify using npx

--- prebuild.sh
+++ prebuild.sh
@@ -2,9 +2,10 @@
 set -e
 set -x
 
-prebuildify --napi --strip --platform darwin --arch x64
-prebuildify --napi --strip --platform linux --arch x64
-prebuildify --napi --strip --platform linux --arch ia32
-prebuildify --napi --strip --platform linux --arch arm64
-prebuildify --napi --strip --platform win32 --arch x64
-prebuildify --napi --strip --platform win32 --arch ia32
+npx prebuildify --napi --strip --platform darwin --arch x64
+npx prebuildify --napi --strip --platform darwin --arch arm64
+npx prebuildify --napi --strip --platform linux --arch x64
+npx prebuildify --napi --strip --platform linux --arch ia32
+npx prebuildify --napi --strip --platform linux --arch arm64
+npx prebuildify --napi --strip --platform win32 --arch x64
+npx prebuildify --napi --strip --platform win32 --arch ia32
