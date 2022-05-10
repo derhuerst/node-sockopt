041ddfda0437dcb34de0271133039605471e85ae
Jannis R <mail@jannisr.de>
2022-05-10 23:50:29 +0200

    prebuild.sh: remove Linux & Windows ia32 💥

--- prebuild.sh
+++ prebuild.sh
@@ -5,7 +5,5 @@ set -x
 npx prebuildify --napi --strip --platform darwin --arch x64
 npx prebuildify --napi --strip --platform darwin --arch arm64
 npx prebuildify --napi --strip --platform linux --arch x64
-npx prebuildify --napi --strip --platform linux --arch ia32
 npx prebuildify --napi --strip --platform linux --arch arm64
 npx prebuildify --napi --strip --platform win32 --arch x64
-npx prebuildify --napi --strip --platform win32 --arch ia32
