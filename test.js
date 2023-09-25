a688af0c6492f8bad83ad91ac4877d85f6f66ef7
Jannis R <mail@jannisr.de>
2023-09-25 13:43:15 +0300

    add IPv4 test ✅

--- test.js
+++ test.js
@@ -4,18 +4,16 @@ const {createSocket} = require('dgram')
 const {strictEqual: eql, notStrictEqual: notEql, ok} = require('assert')
 const {getsockopt, setsockopt} = require('.')
 
+const abortWithError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
 const SOL_SOCKET = 0xffff
 const SO_REUSEADDR = 0x0004
 const SO_SNDBUF = 0x1001
 
-const socket = createSocket({type: 'udp6'})
-
-socket.on('error', (err) => {
-	console.error(err)
-	process.exit(1)
-})
-
-socket.bind(12345, '::', () => {
+const testSocket = (family, socket) => {
 	const reusePort = !!getsockopt(socket, SOL_SOCKET, SO_REUSEADDR)
 	eql(reusePort, false, 'precondition failed: SO_REUSEADDR is not false')
 
@@ -29,6 +27,25 @@ socket.bind(12345, '::', () => {
 	setsockopt(socket, SOL_SOCKET, SO_SNDBUF, 1234)
 	eql(getsockopt(socket, SOL_SOCKET, SO_SNDBUF), 1234, 'setting SO_SNDBUF failed')
 
-	console.info('looks good ✔︎')
-	socket.close()
-})
+	console.info(family + ' looks good ✔︎')
+}
+
+{
+	const socket = createSocket({type: 'udp4'})
+	socket.on('error', abortWithError)
+
+	socket.bind(12345, '0.0.0.0', () => {
+		testSocket('IPv4', socket)
+		socket.close()
+	})
+}
+
+{
+	const socket = createSocket({type: 'udp6'})
+	socket.on('error', abortWithError)
+
+	socket.bind(12345, '::', () => {
+		testSocket('IPv6', socket)
+		socket.close()
+	})
+}
